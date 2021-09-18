7fd40921a516795e6b04b35500280776a5e9393d
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2021-09-18 20:45:59 +0530

    ✅day1

---
+++ solveme.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b= in.nextInt();
+        
+        int c = a + b;
+        
+        System.out.println(c);
+        
+    }
+}
